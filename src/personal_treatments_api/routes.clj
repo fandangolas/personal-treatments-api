429a527fa895008364c769b1349d6ea114f5b8e2
Nícolas <nicolas.knoxx@gmail.com>
2020-10-31 15:40:01 -0300

    creating new medicines routes

--- src/personal_treatments_api/routes.clj
+++ src/personal_treatments_api/routes.clj
@@ -1,7 +1,10 @@
 (ns personal-treatments-api.routes
   (:require [io.pedestal.http.route :as route]
-            [personal-treatments-api.http-in.greeting :as greeting]))
+            [personal-treatments-api.http-in.greeting :as greeting]
+            [personal-treatments-api.http-in.medicines :as medicines]))
 
 (def greeting
   (route/expand-routes
-    #{["/greet" :get greeting/respond-hello :route-name :greet]}))
+    #{["/greet" :get greeting/respond-hello :route-name :greet]
+      ["/medicines" :get medicines/list! :route-name :list-medicines]
+      ["/medicines" :post medicines/create! :route-name :create-medicine]}))
