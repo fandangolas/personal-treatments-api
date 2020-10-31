5e9e6b5b5b8c99a35d5c0adb0207d3a1faebc6a2
Nícolas <nicolas.knoxx@gmail.com>
2020-10-31 16:28:24 -0300

    removing hello-world ns reference

--- src/personal_treatments_api/routes.clj
+++ src/personal_treatments_api/routes.clj
@@ -1,6 +1,5 @@
 (ns personal-treatments-api.routes
   (:require [io.pedestal.http.route :as route]
-            [personal-treatments-api.http-in.greeting :as greeting]
             [personal-treatments-api.http-in.medicines :as medicines]))
 
 (def greeting
